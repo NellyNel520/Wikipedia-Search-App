5a90cfa0cd60d7f9f1b62d68d5d1b4b8dc3f5fea
Nelly <$>
2022-11-17 22:51:21 -0500

    changed console log to display results in handle submit function inorder to display results on the page instead of console

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ async function handleSubmit(event) {
   const searchQuery = inputValue.trim();
   try {
     const results = await searchWikipedia(searchQuery);
-    console.log(results);
+    displayResults(results);
   } catch (err) {
     console.log(err);
     alert('Failed to search wikipedia');
@@ -26,4 +26,27 @@ async function handleSubmit(event) {
     }
     const json = await response.json();
     return json;
+  }
+
+  function displayResults(results) {
+    // Get a reference to the `.js-search-results` element
+    const searchResults = document.querySelector('.js-search-results');
+  
+    // Iterate over the `search` array. Each nested object in the array can be
+    // accessed through the `result` parameter
+    results.query.search.forEach(result => {
+      const url = `https://en.wikipedia.org/?curid=${result.pageid}`;
+  
+      // Append the search result to the DOM
+      searchResults.insertAdjacentHTML(
+        'beforeend',
+        `<div class="result-item">
+          <h3 class="result-title">
+            <a href="${url}" target="_blank" rel="noopener">${result.title}</a>
+          </h3>
+          <a href="${url}" class="result-link" target="_blank" rel="noopener">${url}</a>
+          <span class="result-snippet">${result.snippet}</span><br>
+        </div>`
+      );
+    });
   }
