5398614cd63817b9f6731576bb57bd5d87938add
Nelly <$>
2022-11-17 22:43:08 -0500

    added async function to connect api endpoint

--- app.js
+++ app.js
@@ -11,3 +11,13 @@ function handleSubmit(event) {
   // print `searchQuery` to the console
   console.log(searchQuery);
   }
+
+  async function searchWikipedia(searchQuery) {
+    const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchQuery}`;
+    const response = await fetch(endpoint);
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    const json = await response.json();
+    return json;
+  }
